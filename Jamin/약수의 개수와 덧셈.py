71be2aec00fb904780d220234b579fa7e55226f8
jaminleee <jamin_00@naver.com>
2024-01-18 02:00:48 -0600

    약수의 개수와 덧셈 01/17/24(수)

---
+++ Jamin/약수의 개수와 덧셈.py
@@ -0,0 +1,17 @@
+# 1. 약수 개수 구하기
+# 2. 더하거나 빼기
+
+def solution(left, right):
+    answer = 0
+    
+    for i in range (left, right+1):
+        num = 0
+        for j in range(1,i+1):
+            if i%j ==0:
+                num += 1
+        if num%2 == 0:
+            answer += i
+        else:
+            answer -= i
+                
+    return answer
